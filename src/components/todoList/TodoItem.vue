6f806290fa62eba4923d6f14e960c089d3547fad
zlfera <zlfera@gmail.com>
2021-11-21 18:59:33 +0800

    up

--- src/components/todoList/TodoItem.vue
+++ src/components/todoList/TodoItem.vue
@@ -2,18 +2,18 @@
   <div>
     <input
       type="checkbox"
-      :checked="item!.status === TODO_STATUS.FINISHED"
+      :checked="item!.status === statusState.FINISHED"
       @click="setStatus(item!.id)"
-      :class="item!.status === TODO_STATUS.FINISHED ? 'line-through' : ''"
+      :class="item!.status === statusState.FINISHED ? 'line-through' : ''"
     />
     <span>{{ item!.content }}</span>
     <button @click="removeTodo(item!.id)">删除</button>
     <button
-      v-if="item!.status !==TODO_STATUS.FINISHED"
+      v-if="item!.status !==statusState.FINISHED"
       @click="setDoing(item!.id)"
-      :class="item!.status ===TODO_STATUS.DOING ? 'doing' : 'willdo'"
+      :class="item!.status ===statusState.DOING ? 'doing' : 'willdo'"
     >
-      {{ item!.status ===TODO_STATUS.DOING ? "正在做" : "马上做" }}
+      {{ item!.status ===statusState.DOING ? "正在做" : "马上做" }}
     </button>
   </div>
 </template>
@@ -23,7 +23,27 @@ import { ITodo, TODO_STATUS } from "@/store/modules/todoList/interface";
 import { useTodo } from "@/hooks";
 
 defineProps({ item: Object as PropType<ITodo> });
-const { setStatus, removeTodo, setDoing } = useTodo();
+//const emit = defineEmits(["removeTodo", "setDoing"]);
+const emit = defineEmits<{
+  (event: "removeTodo", id: number): void;
+  (event: "setDoing", id: number): void;
+  (event: "setStatus", id: number): void;
+}>();
+//const { setStatus, removeTodo, setDoing } = useTodo();
+const statusState = {
+  DOING: TODO_STATUS.DOING,
+  FINISHED: TODO_STATUS.FINISHED,
+  WILLDO: TODO_STATUS.WILLDO,
+};
+const removeTodo = (id: number): void => {
+  emit("removeTodo", id);
+};
+const setStatus = (id: number): void => {
+  emit("setStatus", id);
+};
+const setDoing = (id: number): void => {
+  emit("setDoing", id);
+};
 </script>
 
 <style scope>
